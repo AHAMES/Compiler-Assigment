be3e278ff63db47a39a51b8b9e6c7b6d15ebdf60
Ahmed ElSabbagh <Ahmed.h.elsabbgh@gmail.com>
2019-03-16 20:55:18 +0200

    re wrote the code

--- GUI.py
+++ GUI.py
@@ -4,20 +4,9 @@ Created on Wed Mar 13 12:41:25 2019
 
 @author: Ahmed
 """
-
-from tkinter import *
-import tkinter.messagebox
-
-app=Tk()
-
-app.title('GUI Example')
-app.geometry('450x300+200+200')
-
-labelText=StringVar()
-labelText.set("Click Button")
-
-lable1=Label(app,textvariable=labelText, height=4)
-
-lable1.pack()
-
-app.mainloop()
+from PySide2.QtWidgets import QApplication, QLabel
+ 
+app = QApplication([])
+label = QLabel("Hello Qt for Python!")
+label.show()
+app.exec_()
--- Scanners.py
+++ Scanners.py
@@ -19,15 +19,27 @@ reservedTokens={
 def identifiers(inputString):
     newToken=""
     splitPoint=0
-    if str(inputString[0][0]).isalpha() and (inputString[1]=='' or inputString[1]=='Identifier'):
+    if inputString[0][0]=='S' and inputString[1]!='reserved_word':
+        for i in range(0,len(inputString[0])):
+            if inputString[0][i]=='.' or str(inputString[0][i]).isalpha()==True:
+                newToken+=inputString[0][i]
+            else:
+                splitPoint=i
+                if newToken=='System.out.println':
+                    return {'Type':"reserved_word_split",'newToken':newToken,'Split_Point':splitPoint}
+    newToken=""
+    splitPoint=0          
+    if IndentifyTokens(str(inputString[0][0]))=='symbol' and len(inputString[0])>1:
+        return {'Type':"_split",'newToken':newToken,'Split_Point':splitPoint}
+    elif str(inputString[0][0]).isalpha() and (inputString[1]=='' or inputString[1]=='Identifier'):
         for i in range(0,len(inputString[0])):
             if str(inputString[0][i]).isalpha() or str(inputString[0][i]).isdigit() or inputString[0][i]=='_':
                 newToken+=inputString[0][i]
             else:
                 splitPoint=i
-                return ["Identifier",newToken,splitPoint]
-        return ["Identifier",newToken,splitPoint]
-    elif str(inputString[0][0]).isdigit() and str(inputString[1])!='integer':
+                return {'Type':"Identifier_split",'newToken':newToken,'Split_Point':splitPoint}
+        return{'Type':"Identifier",'newToken':newToken,'Split_Point':0}
+    elif str(inputString[0][0]).isdigit() and str(inputString[1])!='Integer':
         for i in range(0,len(inputString[0])):
             if str(inputString[0][i]).isdigit():
                 newToken+=inputString[0][i]
@@ -35,16 +47,19 @@ def identifiers(inputString):
                 newToken="Invalid" 
             else:
                 splitPoint=i
-                return["Invalid",newToken,splitPoint]
+                return{'Type':"Integer_split",'newToken':newToken,'Split_Point':splitPoint}
+        return{'Type':"Integer",'newToken':newToken,'Split_Point':0}
     else:
-        return [False,"",0]       
+        return {'Type':inputString[1],'newToken':"",'Split_Point':0}       
 
 
 def IndentifyTokens(inputString):
+    
     for i in reservedTokens:
         m=False
         if(len(inputString)==len(i)):
             j=0
+            m=True
             for c in inputString:
                 if i[j]!=c:
                     m=False
@@ -52,7 +67,6 @@ def IndentifyTokens(inputString):
                 j+=1
         if m==True:
             return reservedTokens[i]
-    
     return ""
     
 def Tokenization(code):
@@ -106,64 +120,38 @@ def main(File):
     code = open(File, "r")
     Tokens=Tokenization(code)
     size=len(Tokens)
-
+    
     while(True):
         for i in range(0,len(Tokens)):
-            x=identifiers([Tokens[i][0],Tokens[i][1]]) 
-            if x[0]==False and (Tokens[i][1]==""):
+            identifierResult=identifiers([Tokens[i][0],Tokens[i][1]])
+            if 'Invalid' in identifierResult['newToken']:
                 Tokens[i][1]='Invalid'
-                if 'Invalid' not in x[1]:
-                    m=Tokens[i][0].split(Tokens[i][0][x[2]],1)
-                    if(len(m[1])>0 and (str(x[1]).isdigit())==False):
-                        symbol=Tokens[i][0][x[2]]
-                        line=Tokens[i][2]
-                        del Tokens[i] #delete the old token
-                        Tokens.insert(i,[symbol,IndentifyTokens(symbol),line]) #add the splitting symbol
-                        if(m[1]!=''):
-                            Tokens.insert(i+1,[m[1],"Identifier",line]) #add new token to the mix
-                    elif((str(x[1]).isdigit())==True): #'''''''''fix this line'''''''''
-                        if(len(m[1])>0 and (str(Tokens[i][0][x[2]]).isdigit())==False):
-                            symbol=Tokens[i][0][x[2]]
-                            line=Tokens[i][2]
-                            del Tokens[i] #delete the old token
-                            Tokens.insert(i,[x[1],"integer",line]) #add new token to the mix
-                            Tokens.insert(i+1,[symbol,'symbol',line]) #add the splitting symbol
-                            if(m[1]!=''):
-                                Tokens.insert(i+2,[m[1],IndentifyTokens(m[1]),line])
-                    
-            elif x[0]==False and (Tokens[i][1]=="Identifier"):
-                if IndentifyTokens(Tokens[i][0][0])=="symbol":
-                    m=Tokens[i][0].split(Tokens[i][0][x[2]],1)
-                    if(len(m[1])>0 and ~(str(Tokens[i][0][x[2]]).isdigit())):
-                        symbol=Tokens[i][0][x[2]]
-                        line=Tokens[i][2]
-                        del Tokens[i] #delete the old token
-                        Tokens.insert(i,[symbol,'symbol',line]) #add the splitting symbol
-                        Tokens.insert(i+1,[m[1],"Identifier",line]) #add new token to the mix
-            elif x[0]==False and str(x[1]).isdigit():
-                m=Tokens[i][0].split(Tokens[i][0][x[2]],1)
-                if((str(Tokens[i][0][x[2]]).isdigit())==False):
-                    symbol=Tokens[i][0][x[2]]
-                    line=Tokens[i][2]
-                    del Tokens[i] #delete the old token
-                    Tokens.insert(i,[x[1],"integer",line]) #add new token to the mix
-                    Tokens.insert(i+1,[symbol,'symbol',line]) #add the splitting symbol
-                    if(m[1]!=''):
-                        Tokens.insert(i+2,[m[1],IndentifyTokens(m[1]),line])
-            else:
-                if x[2]==0 and Tokens[i][1]=="":
-                    Tokens[i][1]='Identifier'
-                elif x[2]!=0 and (Tokens[i][1]=="" or Tokens[i][1]=="Identifier") :
-                    m=Tokens[i][0].split(Tokens[i][0][x[2]],1) #take the splited part
-                    symbol=Tokens[i][0][x[2]]
-                    line=Tokens[i][2]
-                    del Tokens[i] #delete the old token
-                    Tokens.insert(i,[x[1],"Identifier",line]) #add new token to the mix
-                    Tokens.insert(i+1,[symbol,IndentifyTokens(symbol),line]) #add the splitting symbol
-                    if(m[1]!=''):
-                        Tokens.insert(i+2,[m[1],IndentifyTokens(m[1]),line])
+            elif 'split' not in identifierResult['Type']:
+                if IndentifyTokens(Tokens[i][0])!='':
+                    Tokens[i][1]=IndentifyTokens(Tokens[i][0])
                 else:
-                    continue
+                    Tokens[i][1]=identifierResult['Type']
+            else:
+                line=Tokens[i][2]
+                symbol=Tokens[i][0][identifierResult['Split_Point']]
+                if len(Tokens[i][0])>1 and (symbol=='='):
+                    if Tokens[i][0][identifierResult['Split_Point']+1]=='=':
+                        symbol+='='
+                if len(Tokens[i][0])>1 and(symbol=='&'):
+                    if Tokens[i][0][identifierResult['Split_Point']+1]=='&':
+                        symbol+='&'
+                spliter=Tokens[i][0].split(symbol,1)
+                del Tokens[i]
+                if spliter[0]=='' and spliter[1]!='':
+                    Tokens.insert(i,[symbol,'symbol',line])
+                    Tokens.insert(i+1,[spliter[1],"",line])
+                elif spliter[1]=='' and spliter[0]!='':
+                    Tokens.insert(i,[spliter[0],identifierResult['Type'].replace("_split",""),line])
+                    Tokens.insert(i+1,[symbol,'symbol',line])
+                else:
+                    Tokens.insert(i,[spliter[0],identifierResult['Type'].replace("_split",""),line])
+                    Tokens.insert(i+1,[symbol,'symbol',line])
+                    Tokens.insert(i+2,[spliter[1],"",line])
         if(size!=len(Tokens)):
             size=len(Tokens)
         else:
